5335d7c090b086f6d01c9e900d5ac34600695a63
Amanda Tan <ahkaitan.68@gmail.com>
2018-10-17 01:04:23 +0000

    declares winner

--- js/script.js
+++ js/script.js
@@ -9,11 +9,66 @@
 // DOCUMENT READY FUNCTION BELOW
 $(document).ready(function(){
     
-    $("#shoot").click(function(){
-        $("#userChoice").text($("#input").val());
-    });
-    
     var userChoice="";
     var computerChoice="";
     var winner="";
+    
+    var randomNumber = 0;
+    
+    $("#shoot").click(function(){
+        var userInput = $("#input").val();
+        randomNumber = Math.random();
+        if((userInput === "rock") || (userInput === "scissors") || (userInput === "paper")){
+            $("#userChoice").text(userInput);
+            $("#result").text("");
+        }
+        else{
+            $("#result").text("Don't you know how to play this game?");
+        }
+        if(randomNumber > 0.66){
+            computerChoice = "rock";
+            $("#computerChoice").text("rock");
+        }
+        else if(randomNumber < 0.66 && randomNumber > 0.33){
+            computerChoice = "scissors";
+            $("#computerChoice").text("scissors");
+        }
+        else{
+            computerChoice = "paper";
+            $("#computerChoice").text("paper");
+        }
+        if(userInput === "rock"){
+            if(computerChoice === "rock"){
+                $("#result").text("It's a tie!");
+            }
+            else if(computerChoice === "scissors"){
+                $("#result").text("User wins!");
+            }
+            else if(computerChoice === "paper"){
+                $("#result").text("Computer wins!");
+            }
+        }
+        if(userInput === "scissors"){
+            if(computerChoice === "rock"){
+                $("#result").text("Computer wins!");
+            }
+            else if(computerChoice === "scissors"){
+                $("#result").text("It's a tie!");
+            }
+            else if(computerChoice === "paper"){
+                $("#result").text("User wins!");
+            }
+        }
+        if(userInput === "paper"){
+            if(computerChoice === "rock"){
+                $("#result").text("User wins!");
+            }
+            else if(computerChoice === "scissors"){
+                $("#result").text("Computer wins!");
+            }
+            else if(computerChoice === "paper"){
+                $("#result").text("It's a tie!");
+            }
+        }
+    });
 });
